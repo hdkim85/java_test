7933d58eae702c6c5b03900b0d26897ce2551325
hdkim85 <hdkim85@gmail.com>
2020-05-01 15:13:23 +0900

    test

---
+++ src/test/DebugTest.java
@@ -0,0 +1,15 @@
+package test;
+
+public class DebugTest {
+
+	public static void main(String[] args) {
+
+		System.out.println("sysout되서 좋음");
+		System.out.println("sysout되서 진짜 좋음");
+		System.out.println("sysout되서 엄청 좋음");
+		System.out.println("sysout되서 정말 좋음");
+		System.out.println("sysout되서 완전 좋음");
+		
+	}
+
+}
